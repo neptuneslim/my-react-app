fa16ccd8d8e892989bce2d78000d574ec8ad093c
neptuneslim <melvorooks@gmail.com>
2021-05-04 22:25:07 -0500

    New Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,13 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Learn about all Streets of Rage 4 characters</h1>
-    <p>Complete site about the coolest characters.</p>
-    <Link to="/page-2/">Watch the video dude</Link>
+    <div className="Hero">
+     <div className="HeroGroup">
+      <h1>Learn about all Streets of Rage 4 characters</h1>
+      <p>Complete site about the coolest characters.</p>
+      <Link to="/page-2/">Watch the video dude</Link>
+     </div>
+    </div>
   </div>
 )
 
